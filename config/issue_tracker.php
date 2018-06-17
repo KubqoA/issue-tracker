80893362a84c833519b5d02b953dd202b718602a
Jakub Arbet <arbetjakub@gmail.com>
2018-06-17 20:14:02 +0200

    Add config

---
+++ config/issue_tracker.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Hosting Service Settings
+     |--------------------------------------------------------------------------
+     |
+     | Issue tracker is by default set to use the Gitea API, if you want to use
+     | any other git repository hosting service please specify it here.
+     |
+     | Currently supported options are: gitea, gogs
+     |
+     */
+
+    'hosting_service' => 'gitea',
+
+    /*
+     |--------------------------------------------------------------------------
+     | API settings
+     |--------------------------------------------------------------------------
+     |
+     | This package uses API of your selected hosting service to add the issues
+     | to the repo. For this the package requires the root url of your hosting
+     | service, the repository name and the repository owner's name. We also
+     | need access token so we can use the api
+     |
+     */
+
+    'api' => [
+        'url' => 'https://try.gitea.io/',
+        'owner_name' => 'owner',
+        'repository_name' => 'example',
+        'access_token' => env('ISSUE_TRACKER_ACCESS_TOKEN'),
+    ],
+
+];
--- src/IssueTrackerServiceProvider.php
+++ src/IssueTrackerServiceProvider.php
@@ -7,13 +7,27 @@ use Illuminate\Support\ServiceProvider;
 class IssueTrackerServiceProvider extends ServiceProvider
 {
     /**
+     * The path to the package's config
+     *
+     * @var string
+     */
+    protected $packageConfigPath = __DIR__ . '/../config/issue_tracker.php';
+
+    /**
+     * The default config key used for publishing and merging
+     *
+     * @var string
+     */
+    protected $configKey = 'issue_tracker';
+
+    /**
      * Bootstrap the application services
      *
      * @return void
      */
     public function boot()
     {
-        //
+        $this->publishConfig();
     }
 
     /**
@@ -23,6 +37,16 @@ class IssueTrackerServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->mergeConfigFrom($this->packageConfigPath, $this->configKey);
+    }
+
+    /**
+     * Publishes the config file
+     */
+    private function publishConfig()
+    {
+        $this->publishes([
+            $this->packageConfigPath => config_path($this->configKey . '.php'),
+        ], 'config');
     }
 }
